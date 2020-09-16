2c6286aa47852742e321111c391184df15dea186
Alexandru Adam <dm_alexandru@yahoo.com>
2020-09-16 14:35:20 +0300

    Changed the init file so that is more organized and scalable

--- __init__.py
+++ __init__.py
@@ -1,24 +1,36 @@
 # imports
 from flask import Flask,render_template, url_for
+import os
 
-#app init
-app = Flask(__name__)
+def create_app(test_config=None):
+	app = Flask(__name__,instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY="dev"
+		)
+	if test_config is None:
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		app.config.from_mapping(test_config)
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
 
+	@app.route("/index")
+	@app.route("/")
+	def index():
+			return render_template("index.html")
 
-#web pages
+	@app.route("/login/")
+	def login():
+		return render_template("login.html")
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/login/")
-def login():
-	return render_template("login.html")
+	@app.route("/register/")
+	def register():
+		return render_template('register.html')
 	
-@app.route("/register/")
-def register():
-	return render_template('register.html')
+	return app
 
-if __name__ == '__main__':
-	app.run(debug=True)
+
+
+
