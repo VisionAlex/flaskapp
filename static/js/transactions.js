e3b2cee99a87a09702b84be042fe7609234e7c26
Alexandru Adam <dm_alexandru@yahoo.com>
2020-11-06 16:55:18 +0000

    Fixed the transactions page javascript

--- static/js/transactions.js
+++ static/js/transactions.js
@@ -1,48 +1,95 @@
-const asset_type = document.querySelector("#asset_type")
-const symbol = document.querySelector("#symbol")
-const name = document.querySelector("#name")
-const units = document.querySelector("#units")
-const price = document.querySelector("#price")
-const currency = document.querySelector("#currency")
-const fx_rate = document.querySelector("#fx_rate")
-const fx_help = document.querySelector("#fx_help")
+const asset_type = document.querySelector("#asset_type");
+const symbol = document.querySelector("#symbol");
+const name = document.querySelector("#name");
+const units = document.querySelector("#units");
+const price = document.querySelector("#price");
+const currency = document.querySelector("#currency");
+const fx_rate = document.querySelector("#fx_rate");
+const fx_help = document.querySelector("#fx_help");
 
-asset_type.onchange = function() {
-	if (asset_type.value == "Cash") {
-		symbol.hidden = true;
-		name.hidden = true;
-		units.placeholder = "Amount";
-		price.placeholder = "FX Rate";
+asset_type.addEventListener('change', result)
 
-	} else if (asset_type.value == "Real_Estate") {
-		symbol.hidden = false;
-		name.hidden = false;
-		units.hidden = true;
-		price.placeholder="Price";
-	} else if (asset_type.value == "Gold") {
-		symbol.hidden = false;
-		symbol.placeholder = "Unit type(g, oz, paper)"
-		name.hidden = true;
-		units.hidden = false;
-		price.placeholder = "Price"
-		units.placeholder = "Units"
+function result(){
 
-	} else if (asset_type.value == "Stocks") {
-		symbol.hidden= false;
-		symbol.placeholder = "Symbol"
-		symbol.required = true;
-		name.hidden=false;
-		units.hidden = false;
-		units.placeholder = "Shares"
-		price.placeholder = "Price"
-	} else if (asset_type.value == "bonds") {
-		units.placeholder = "Units"
-		symbol.hidden = false;
-		symbol.placeholder = "Symbol"
-		name.hidden = false;
-		units.hidden = false;
-	}	
+	switch (asset_type.value) {
+		case "Cash":
+			symbol.hidden = true;
+			name.hidden = true;
+			units.placeholder = "Amount";
+			price.placeholder = "FX Rate";
+			break;
+		case "Real Estate":
+			symbol.hidden = false;
+			name.hidden = false;
+			units.hidden = true;
+			symbol.placeholder = "ex: ESTATE1";
+			price.placeholder ="Price";
+			break;
+		case "Gold":
+			symbol.hidden = false;
+			symbol.placeholder = "Unit type(g, oz, paper)";
+			name.hidden = true;
+			units.hidden = false;
+			price.placeholder = "Price";
+			units.placeholder = "Units";
+			break;
+		case "Stocks":
+			symbol.hidden = false;
+			symbol.placeholder = "Symbol";
+			symbol.required = true;
+			name.hidden = false;
+			units.hidden = false;
+			units.placeholder = "Shares";
+			price.placeholder = "Price";
+			break;
+		default:
+			symbol.hidden = false;
+			price.placeholder = "Price";
+			units.hidden = false;
+			name.hidden = false;
+			symbol.required = false;
+			symbol.placeholder = "";
+
+
+	}
 }
+
+// asset_type.onchange = function() {
+// 	if (asset_type.value == "Cash") {
+// 		symbol.hidden = true;
+// 		name.hidden = true;
+// 		units.placeholder = "Amount";
+// 		price.placeholder = "FX Rate";
+
+// 	} else if (asset_type.value == "Real Estate") {
+// 		symbol.hidden = false;
+// 		name.hidden = false;
+// 		units.hidden = true;
+// 		price.placeholder="Price";
+// 	} else if (asset_type.value == "Gold") {
+// 		symbol.hidden = false;
+// 		symbol.placeholder = "Unit type(g, oz, paper)"
+// 		name.hidden = true;
+// 		units.hidden = false;
+// 		price.placeholder = "Price"
+// 		units.placeholder = "Units"
+
+// 	} else if (asset_type.value == "Stocks") {
+// 		symbol.hidden= false;
+// 		symbol.placeholder = "Symbol"
+// 		symbol.required = true;
+// 		name.hidden=false;
+// 		units.hidden = false;
+// 		units.placeholder = "Shares"
+// 		price.placeholder = "Price"
+// 	} else if (asset_type.value == "bonds") {
+// 		units.placeholder = "Units"
+// 		symbol.hidden = false;
+// 		symbol.placeholder = "Symbol"
+// 		name.hidden = false;
+// 		units.hidden = false;
+// 	}	
+// }
 
 currency.onchange = function() {
 	if (currency.value != "EUR") {
