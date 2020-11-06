8e085804f796ad21da68eae0fd18d6bffcfefa6a
Alexandru Adam <dm_alexandru@yahoo.com>
2020-11-06 16:55:46 +0000

    Fixed the transactions page javascript

--- static/js/transactions.js
+++ static/js/transactions.js
@@ -54,42 +54,6 @@ function result(){
 	}
 }
 
-// asset_type.onchange = function() {
-// 	if (asset_type.value == "Cash") {
-// 		symbol.hidden = true;
-// 		name.hidden = true;
-// 		units.placeholder = "Amount";
-// 		price.placeholder = "FX Rate";
-
-// 	} else if (asset_type.value == "Real Estate") {
-// 		symbol.hidden = false;
-// 		name.hidden = false;
-// 		units.hidden = true;
-// 		price.placeholder="Price";
-// 	} else if (asset_type.value == "Gold") {
-// 		symbol.hidden = false;
-// 		symbol.placeholder = "Unit type(g, oz, paper)"
-// 		name.hidden = true;
-// 		units.hidden = false;
-// 		price.placeholder = "Price"
-// 		units.placeholder = "Units"
-
-// 	} else if (asset_type.value == "Stocks") {
-// 		symbol.hidden= false;
-// 		symbol.placeholder = "Symbol"
-// 		symbol.required = true;
-// 		name.hidden=false;
-// 		units.hidden = false;
-// 		units.placeholder = "Shares"
-// 		price.placeholder = "Price"
-// 	} else if (asset_type.value == "bonds") {
-// 		units.placeholder = "Units"
-// 		symbol.hidden = false;
-// 		symbol.placeholder = "Symbol"
-// 		name.hidden = false;
-// 		units.hidden = false;
-// 	}	
-// }
 
 currency.onchange = function() {
 	if (currency.value != "EUR") {
