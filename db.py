a1cd09c604301703aac311282fc0cac719af791b
Alexandru Adam <dm_alexandru@yahoo.com>
2021-01-05 14:01:22 +0200

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import pymysql.cursors
 
 def connection():
 	conn = pymysql.connect(host='graurului.tplinkdns.com',
-						   user='visionalex',
-						   password='Babadochia0@',
-						   db='portfolio_tracker',
+						   user='',
+						   password='',
+						   db='',
 						   charset='utf8mb4',
 						   cursorclass=pymysql.cursors.DictCursor)
 	c = conn.cursor()
